c9b90ed50db0a35941e3ff86b5feb25025b3a535
oelorduy@gmail.com <oelorduy@gmail.com>
2025-05-15 20:34:33 -0500

    Segundo commit

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -36,7 +36,7 @@ export const Formulario = ({
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Validacionesss
+    // Validacionessss
     if (nombre.trim() === "") {
       alert("El campo nombre está vacío");
       setError(true);
